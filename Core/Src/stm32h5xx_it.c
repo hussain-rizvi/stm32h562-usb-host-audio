db6ca504370a2ce5c2a4f5894acbc4a7236938be
Hussain Rizvi <hussain.rizvi1994@outlook.com>
2026-03-19 19:16:00 +0500

    added sdmmc interrupt

--- Core/Src/stm32h5xx_it.c
+++ Core/Src/stm32h5xx_it.c
@@ -55,6 +55,7 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
+extern SD_HandleTypeDef hsd1;
 extern HCD_HandleTypeDef hhcd_USB_DRD_FS;
 extern TIM_HandleTypeDef htim2;
 
@@ -186,6 +187,20 @@ void USB_DRD_FS_IRQHandler(void)
   /* USER CODE BEGIN USB_DRD_FS_IRQn 1 */
 
   /* USER CODE END USB_DRD_FS_IRQn 1 */
+}
+
+/**
+  * @brief This function handles SDMMC1 global interrupt.
+  */
+void SDMMC1_IRQHandler(void)
+{
+  /* USER CODE BEGIN SDMMC1_IRQn 0 */
+
+  /* USER CODE END SDMMC1_IRQn 0 */
+  HAL_SD_IRQHandler(&hsd1);
+  /* USER CODE BEGIN SDMMC1_IRQn 1 */
+
+  /* USER CODE END SDMMC1_IRQn 1 */
 }
 
 /* USER CODE BEGIN 1 */
